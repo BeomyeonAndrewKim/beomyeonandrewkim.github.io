e31dbe93f661e4817331df81583282a73c55b976
BeomyeonAndrewKim <beom911@gmail.com>
2018-03-22 21:09:05 +0900

    Header default title 변경

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -8,7 +8,7 @@ class Layout extends React.Component {
 
     return (
       <div className="layout">
-        <Helmet defaultTitle="Blog by John Doe" />
+        <Helmet defaultTitle="Andrea's Devlog" />
         {children()}
       </div>
     );
